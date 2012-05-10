84f2fa2e537bb8c3cbd539408f756e8ac1fe427b
William DURAND <william.durand1@gmail.com>
2012-05-11 01:43:22 +0200

    Added config files

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+set_include_path(__DIR__ . '/../vendor/phing/phing/classes' . PATH_SEPARATOR . get_include_path());
+
+require_once __DIR__ . '/../vendor/propel/propel1/generator/lib/util/PropelQuickBuilder.php';
