e67a3c5f4a65b3ba0983711d779c107ddbfaf86d
Jan Bláha <Jan.Blaha@iveco.com>
2013-04-09 16:26:17 +0200

    Add break statement to avoid execution of unwanted post hooks.

--- src/templates/objectPostHooks.php
+++ src/templates/objectPostHooks.php
@@ -1,9 +1,7 @@
-
 switch ($this-><?php echo $stateColumnGetter ?>()) {
 <?php foreach ($states as $stateConstant => $postHookMethodName) : ?>
     case static::<?php echo $stateConstant ?>:
         $this-><?php echo $postHookMethodName ?>($con);
-
+        break;
 <?php endforeach; ?>
-    default:
 }
