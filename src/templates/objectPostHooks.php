0fbf84c986d058fb97ca2dc0f091ffa0c40dc01a
Jan Bláha <Jan.Blaha@iveco.com>
2013-04-09 16:28:11 +0200

    Execute post hooks only if the state column has been modified.

--- src/templates/objectPostHooks.php
+++ src/templates/objectPostHooks.php
@@ -1,7 +1,10 @@
-switch ($this-><?php echo $stateColumnGetter ?>()) {
+if ($isStateColumnModified)
+{
+    switch ($this-><?php echo $stateColumnGetter ?>()) {
 <?php foreach ($states as $stateConstant => $postHookMethodName) : ?>
-    case static::<?php echo $stateConstant ?>:
-        $this-><?php echo $postHookMethodName ?>($con);
-        break;
+        case static::<?php echo $stateConstant ?>:
+            $this-><?php echo $postHookMethodName ?>($con);
+            break;
 <?php endforeach; ?>
+    }
 }
